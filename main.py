e310528c977f311364fe9580db53c81f3c38fd9c
andrei.zavadskij <andrey.dev@betzo.net>
2026-06-29 16:29:54 +0400

    fix

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ dp = Dispatcher()
 dp.include_router(router)
 
 if LOCAL_API_URL:
-    _api_server = TelegramAPIServer.from_base(LOCAL_API_URL, is_local=False)
+    _api_server = TelegramAPIServer.from_base(LOCAL_API_URL, is_local=True)
 else:
     _api_server = None
 
